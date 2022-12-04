561479099a67d166e8fac369bcbecf9d7518be34
Abdoulayek <89024805+Abdoulayek@users.noreply.github.com>
2022-12-04 03:52:19 +0100

    Update ClientController.php

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Client;
-use App\Form\ClientType;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,6 +15,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
+    
+    
     //Retourner les leads positifs
     
     #[Route('/client', name: 'app_client', methods: ['GET'])]
@@ -26,5 +28,68 @@ class ClientController extends AbstractController
         return new JsonResponse($donnees, 200);
 
     }
+ 
+    
+    // Mettre les numéros au bon format 10 digit
+    
+    
+    #[Route('/numeroformat', name: 'app_numeroformat', methods: ['GET'])]
+public function numeroformat(ManagerRegistry $doctrine,ClientRepository $clientRepository): Response
+{
+    
+    $em = $doctrine->getManager();
+    $rep = $clientRepository->findAll();
+    foreach( $rep as $val)
+{
+    //Vérifier si le numéro est supérieur à 10
+
+      if(strlen($val->getTelephone())>10)
+       {
+        $tab1 = ['0033','+33'];
+        $tab2 = ['0','0'];
+        $val->setCiv($val->getCiv())
+           ->setNom($val->getNom())
+           ->setPrenom($val->getPrenom())
+           ->setEmail($val->getEmail())
+           ->setCodePostal($val->getCodePostal())
+           ->setIdContact($val->getIdContact())
+           ->setEcheanceProjet($val->getEcheanceProjet())
+           ->setStatut($val->getStatut())
+           ->setCodeConcession($val->getCodeConcession())
+           ->setTelephone(str_replace($tab1,$tab2,$val->getTelephone()));
+       }
+           
+       //Vérifier et identifier le numéro inférieur à 10 ( faux_numéro ) 
+
+    else if (strlen($val->getTelephone())< 10) 
+    {
+        echo "faux numéro ";
+        echo  $val->getIdContact();
+    } 
+    
+// Vérifier si le numéro contient des caractères spéciaux et remplacer 
+
+    $tab1 = ['.','-',' ','/'];
+    $tab2 = ['','','',''];
+
+    $val->setCiv($val->getCiv())
+           ->setNom($val->getNom())
+           ->setPrenom($val->getPrenom())
+           ->setEmail($val->getEmail())
+           ->setCodePostal($val->getCodePostal())
+           ->setIdContact($val->getIdContact())
+           ->setEcheanceProjet($val->getEcheanceProjet())
+           ->setStatut($val->getStatut())
+           ->setCodeConcession($val->getCodeConcession())
+           ->setTelephone(str_replace($tab1,$tab2,$val->getTelephone()));
+    }
+
+    // Remplacer dans la base de données
+    
+    $em->flush();
+    $clientRepository->save($val, true);
+    dd($val);
+    }
+
 
 }
